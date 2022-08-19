96ccf9f603db183a7495a4c0d80a5072d35102aa
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2022-08-19 19:50:41 +0530

    drivers: Nuke gud drivers

--- drivers/char/adsprpc_compat.c
+++ drivers/char/adsprpc_compat.c
@@ -119,15 +119,6 @@ struct compat_fastrpc_ioctl_perf {	/* kernel performance data */
 	compat_uptr_t keys;
 };
 
-<<<<<<< HEAD
-#define FASTRPC_CONTROL_LATENCY		(1)
-struct compat_fastrpc_ctrl_latency {
-	compat_uint_t enable;	/* latency control enable */
-	compat_uint_t level;	/* level of control */
-};
-
-#define FASTRPC_CONTROL_SMMU		(2)
-=======
 #define FASTRPC_CONTROL_LATENCY (1)
 struct compat_fastrpc_ctrl_latency {
 	compat_uint_t enable;
@@ -135,7 +126,6 @@ struct compat_fastrpc_ctrl_latency {
 };
 
 #define FASTRPC_CONTROL_SMMU (2)
->>>>>>> 41a247ee026560b529fdc7b5d9180b1767eac69d
 struct compat_fastrpc_ctrl_smmu {
 	compat_uint_t sharedcb;
 };
